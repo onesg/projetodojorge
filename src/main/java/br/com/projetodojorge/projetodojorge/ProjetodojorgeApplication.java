c53843866a514c43c821c15e66a177d2ec3bbf97
onesg <gabriel.seno@fatec.sp.gov.br>
2022-05-05 21:34:02 -0300

    Primeiro commit

---
+++ src/main/java/br/com/projetodojorge/projetodojorge/ProjetodojorgeApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetodojorge.projetodojorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodojorgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodojorgeApplication.class, args);
+	}
+
+}
